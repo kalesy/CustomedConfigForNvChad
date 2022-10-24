8293455207ff2f10ed21e685e144218caad3e9e1
xujunqi <285361731@qq.com>
2022-10-24 18:26:27 +0800

    change theme and mappings

--- mappings.lua
+++ mappings.lua
@@ -1,4 +1,14 @@
-local mappings = {
-  ["t"] = {"<cmd> NvimTreeToggle <CR>", "Toggle nvimtree", opts = {}},
+local M = {}
+M.disabled = { }
+M.nvterm = { }
+M.general = {
+    -- navigate within insert mode
+    n = {
+        -- switch between windows
+        ["<A-h>"] = { "<C-w>h", "window left" },
+        ["<A-l>"] = { "<C-w>l", "window right" },
+        ["<A-j>"] = { "<C-w>j", "window down" },
+        ["<A-k>"] = { "<C-w>k", "window up" },
+    }
 }
-return mappings
+return M
